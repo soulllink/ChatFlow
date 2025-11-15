aacb15603785682e7213e0e75b54d5e14022f8fb
soulllink <soullinkslsl@yandex.ru>
2025-11-15 21:12:50 +0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  // ←←← ЭТО ОБЯЗАТЕЛЬНО ИЗМЕНИТЬ!
+  base: "/ChatFlow/", // Именно так, с большой C и слешем на конце
+
   build: {
     outDir: "dist",
     assetsDir: "assets",
